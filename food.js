ac646316eaa2676cdd2f20053c7fc71effd0aa94
HeoSion <tldhs123e@gmail.com>
2024-12-04 20:01:23 +0900

    Add files via upload

--- food.js
+++ food.js
@@ -5,3 +5,16 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+    // 모든 restaurant-card 요소를 선택
+    const restaurantCards = document.querySelectorAll('.restaurant-card');
+    
+    // 각 카드에 클릭 이벤트 리스너 추가
+    restaurantCards.forEach(function(card) {
+        card.addEventListener('click', function() {
+            // 클릭된 카드에서 data-url 속성값을 가져와서 링크로 이동
+            const mapUrl = card.getAttribute('data-url');
+            window.open(mapUrl, '_blank'); // 새 창이 아닌 같은 창에서 열리도록 설정
+        });
+    });
+});
